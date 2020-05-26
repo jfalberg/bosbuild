ab0142dddb49c063858562646d95a2cf744b04e6
jfalberg <32420584+jfalberg@users.noreply.github.com>
2020-05-26 17:42:08 -0400

    Add files via upload
    npoints contains built in sql function to calculate points

---
+++ nPoints.sql
@@ -0,0 +1,35 @@
+﻿DELIMITER $$
+
+DROP FUNCTION IF EXISTS `bbdata`.`nPoints` $$
+CREATE DEFINER=`root`@`localhost` FUNCTION `nPoints`(a INT, b INT, c INT, d INT) RETURNS int(11)
+BEGIN
+  DECLARE tmp int;
+  if (a > b) then
+     set tmp = a;
+     set a = b;
+     set b = tmp;
+  end if;
+  if (c > d) then
+     set tmp = c;
+     set c = d;
+     set d = tmp;
+  end if;
+  if (a > c) then
+     set tmp = a;
+     set a = c;
+     set c = tmp;
+  end if;
+  if (b > d) then
+     set tmp = b;
+     set b = d;
+     set d = tmp;
+  end if;
+  if (b > c) then
+     set tmp = b;
+     set b = c;
+     set c = tmp;
+  end if;
+  RETURN a * 1 + b * 2 + c * 3 + d * 4;
+END $$
+
+DELIMITER ;
